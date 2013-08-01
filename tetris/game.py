277bd6fa9deb0f12072e78917e25820e2866aa98
Shawn Bao <zhenbao@marvell.com>
2013-08-01 22:43:10 +0800

    [Tetris] Add resource
    1. board: 25+3
    2. room: Rect(15,14,282,507)
    
    Signed-off-by: Shawn Bow <shawnbow81@qq.com>

--- tetris/game.py
+++ tetris/game.py
@@ -7,20 +7,31 @@ import pygame
 from pygame.locals import *
 
 class Room(object):
+
+    # We have 10*20 space for boards
     Width, Heigth = (10,20)
+
     def __init__(self):
-        self.clear_space()
+        self.space = list([1]*Room.Width*Room.Heigth)
     
-    def set_space(self, x, y, value):
+    def set_space(self, (x, y, value)):
         if (x >= Room.Width or y <= Room.Heigth):
             pass
-        self.space[x+y*Room.Height] = value
+        self.space[x+y*Room.Width] = value
+        print 'Board (%s,%s)'%(x, y) + ' set to %s' %value
     
-    def get_space(self, x, y, value):
-    
-    def clear_space(self):
-        self.space = [0 for i in range(Room.Width * Room.Heigth)]
-        print r"Room's space is clear", 
+    def get_space(self, (x, y)):
+        return self.space[x+y*Room.Width]
+
+    def empty_space(self):
+        for i in range(Room.Width * Room.Heigth):
+            self.space[i] = 0
+        print r"Room's space is clear" 
+
+class Board(object):
+    def __init__(self):
+        self.count = 4
+        self.color = (255, 0, 0)
 
 
 class Game(object):
@@ -28,10 +39,18 @@ class Game(object):
         self.screen = screen
         self.clock = pygame.time.Clock()
         
+        self.room = Room()
+        self.room.set_space((0, 0, 1))
+        self.room.set_space((0, 1, 1))
         # Create surface for display
         #self.backsurface = pygame.Surface(self.screen.get_size())
         self.background = pygame.image.load('background.bmp').convert()
         
+    def draw_room(self):
+        for x in range(Room.Width):
+            for y in range(Room.Heigth):
+                if self.room.get_space((x, y)) != 0:
+                    pygame.draw.rect(self.screen, (255,0,0) , Rect(x*20+3*(x+1),426-(y+1)*20-3*(y+1),20,20))
     
     def run(self):
         while True:
@@ -42,6 +61,7 @@ class Game(object):
 
             #self.backsurface.blit(self.background,(0,0))
             self.screen.blit(self.background,(0,0))
+            self.draw_room()
             pygame.display.flip()
             self.clock.tick(40)
         pass
@@ -49,3 +69,6 @@ class Game(object):
 
 if __name__ == '__main__':
     r = Room()
+    r.empty_space()
+    r.set_space((0, 19, 4))
+    r.get_space((0, 19))
