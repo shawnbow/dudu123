420fc22382e8e4a449af903e9b26aca290417313
Shawn Bao <zhenbao@marvell.com>
2013-08-09 16:26:41 +0800

    Add Update vendor marvell private prjects script
    Signed-off-by: Shawn Bow <shawnbow81@qq.com>

---
+++ pytest/src/update_vendor_marvell_private.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python
+import os
+
+extra_projects = \
+    ['vendor/marvell/private/android-builder',
+     'vendor/marvell/private/customer-config',
+     'vendor/marvell/private/deployconf',
+     'vendor/marvell/private/metaconfig',
+     'vendor/marvell/private/mkupdate',
+     'vendor/marvell/private/mlsdk',
+     'vendor/marvell/private/product-release'
+    ]
+
+prefix_url = 'http://sh2-git01.marvell.com/git/'
+
+os.system('mkdir -p vendor/marvell/private')
+
+for project in extra_projects:
+    if os.path.exists(project+'/.git'):
+        os.system('cd ' + project + ';' + 'git pull --rebase')
+    else:
+        os.system('git clone ' + prefix_url + project + ' ' + project)
+# 
+# 
+# pdir = os.popen('find . -name .git')
+# print pdir
+# for str in pdir:
+#     str = str.replace('.git\n', '')
+#     os.system('cd ' + str + ';' + 'git pull --rebase')
